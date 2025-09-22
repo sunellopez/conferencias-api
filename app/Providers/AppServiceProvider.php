f6599d14caa2cff2ad57cc8374edea479cbd92c1
sunellopez <sunellopez@gmail.com>
2025-09-22 14:07:00 -0600

    Inicializando repo directamente en conferencias-api

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Carbon\CarbonInterval;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+
+        Passport::tokensExpireIn(CarbonInterval::days(15));
+        Passport::refreshTokensExpireIn(CarbonInterval::days(30));
+        Passport::personalAccessTokensExpireIn(CarbonInterval::months(6));
+    }
+}
